0194cf1a449aefadf89f47e11866c656bba8ca50
kozistr <kozistr@gmail.com>
2019-10-27 18:18:24 +0900

    update: separate into build_network/model

--- model.py
+++ model.py
@@ -25,9 +25,10 @@ class MaskGuidedPortraitEditing:
         self.net_left_eye_projector = None
         self.net_right_eye_projector = None
 
+        self.build_networks()
         self.build_models()
 
-    def build_models(self):
+    def build_networks(self):
         # Local Embedding Sub-Network # Encoder
         self.net_face_encoder = build_component_encoder(input_shape=(3, 256, 256), norm_type=self.norm_type)
         self.net_hair_encoder = build_component_encoder(input_shape=(3, 256, 256), norm_type=self.norm_type)
@@ -48,6 +49,11 @@ class MaskGuidedPortraitEditing:
         self.net_mouth_projector = build_component_projector(input_shape=(3, 80, 144), norm_type=self.norm_type)
         self.net_left_eye_projector = build_component_projector(input_shape=(3, 32, 40), norm_type=self.norm_type)
         self.net_right_eye_projector = build_component_projector(input_shape=(3, 32, 40), norm_type=self.norm_type)
+
+        # Discriminators
+
+    def build_models(self):
+        pass
 
     def __str__(self):
         return "Mask-Guided Portrait Editing w/ cGAN"
